22e783cd3fad8324cc41bdbcbb3d324317ee21c3
YuriiYakimenko <61474517+YuriiYakimenko@users.noreply.github.com>
2025-07-15 10:00:28 +0300

    Script_v3
    Працює коректно

--- Assets/Scripts/ControllingCue_v3.cs
+++ Assets/Scripts/ControllingCue_v3.cs
@@ -40,33 +40,15 @@ public class NewMonoBehaviourScript2 : MonoBehaviour
             }
             if (Input.GetMouseButton(0))
             {
-                // Отримуємо точку на площині столу під мишкою
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                Plane tablePlane = new Plane(Vector3.up, balls[0].transform.position); // площина столу (Y)
-                float enter;
-                if (tablePlane.Raycast(ray, out enter))
-                {
-                    Vector3 mouseWorldPos = ray.GetPoint(enter);
-
-                    // Вектор від кулі до миші
-                    Vector3 dir = (mouseWorldPos - balls[0].transform.position).normalized;
-
-                    // Позиція кия на відстані 1.5f від кулі у напрямку миші
-                    cueVisual.transform.position = balls[0].transform.position + dir * 1.5f;
-
-                    // Повертаємо кий на кулю
-                    cueVisual.transform.LookAt(balls[0].transform.position, Vector3.up);
-                }
+                float deltaX = Input.mousePosition.x - mouseStartPos.x;
+                transform.RotateAround(balls[0].transform.position, Vector3.up, deltaX * 0.2f);
+                mouseStartPos = Input.mousePosition;
             }
             if (Input.GetMouseButtonUp(0))
             {
                 Vector3 hitDirection = (balls[0].transform.position - cueVisual.transform.position).normalized;
                 balls[0].AddForce(hitDirection * strikeForce, ForceMode.Impulse);
             }
-            // if (Input.GetMouseButtonUp(0))
-            // {
-            //     balls[0].AddForce(Quaternion.AngleAxis(90f, Vector3.back) * cueVisual.transform.forward * strikeForce, ForceMode.Impulse);
-            // }
         }
     }
 }
